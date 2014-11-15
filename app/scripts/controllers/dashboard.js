a6921d78efdbaf496b84b9ef0620ebd70ecd54dc
Marius Miron <marius.costel.miron@gmail.com>
2014-11-15 13:53:01 +0200

    fixing bug for dashboard

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -11,7 +11,11 @@
  * # AboutCtrl
  * Controller of the whosfilmingFrontendApp
  */
-angular.module('whosfilmingFrontendApp', [])
-    .controller('DashboardCtrl', ["$scope", function($scope) {
-        $scope.message = "Yoyoyo bro";
-    }]);
+angular.module('whosfilmingFrontendApp')
+    .controller('DashboardCtrl', function($scope) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    });
